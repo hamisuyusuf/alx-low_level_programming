c76495120004f63ca3703dcbfb704f6b8b12d91c
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-09 04:42:37 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilinual puzzle");
 	return (0);
 
 }
