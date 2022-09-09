02a4a1397b148c4d864b5cce8ad67a51a14d92fb
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-09 07:22:29 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- *
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-    int a;
-    long int b;
-    long long int c;
-    float f
-    char d;
-    
+        char d;
+	int a;
+        long int b;
+        long long int c;
+        float f;
 
-   	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0); 
-}	
+        printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+        return (0);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                14,6-13       All
