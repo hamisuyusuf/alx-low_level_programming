8557292936a01e1127e8f7e89d9f2448d88465d7
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-09 23:58:00 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * main -
+ * main - Entry point
  *
  * Description: print the value of n:
- * print greater than, is zero and is not less than 6.
+ *	 print greater than, is zero and is not less than 6.
  *
  * Return: 0
  */ 
