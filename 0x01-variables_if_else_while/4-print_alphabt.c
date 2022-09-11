409211949f5a7578f15f6d4b836c62466d60a48c
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-10 17:43:09 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Get a random number and check its last digit, compare it with 5 
+ * Description: print all aplhabet letters except q and e
  * Return: 0
  */
 int main(void)
