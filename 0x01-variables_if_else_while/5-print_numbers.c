c0ef5fcbe64a1be2ed887e5191af3bd1637ea306
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-10 18:00:43 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- * Description:  a program that prints all single digit numbers of base 10 starting from 0
+ * Description: a program that prints all single digit numbers of base 10 starting from 0
  * Return: 0
  */
 int main(void)
 {
-	int b = 0 ;
+	int b = '0' ;
        	
 	
-	while (b <= 9 )
+	while (b <= '9' )
 	{
 		putchar(b);
 		b++;
@@ -19,5 +19,5 @@ int main(void)
 
 	putchar('\n');
 
-	return(0);
+return (0);
 }
