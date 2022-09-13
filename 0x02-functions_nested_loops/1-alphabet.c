12bbade3daea3a93b11c38f56b8637c1a5feaec1
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-13 15:42:27 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * main - check the code
+ *
+ * Description: print alphabet lower case all
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar("\n");
+}
