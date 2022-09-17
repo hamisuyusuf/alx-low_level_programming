08835bffc39913737530e55416233098270f8afc
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-17 16:06:07 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers - function that prints num form 0 to 9
+ * Return: return 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+
+		_putchar (i);
+	}
+	_putchar ("\n");
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_iiH
 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char);
 void print_numbers(void);
 void more_numbers(void);
 void print_line(int n);
