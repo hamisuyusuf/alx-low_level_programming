b42950bbcc379ddc8ae832fdae5e9faec32e5c7c
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-19 15:45:41 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * _strlen - functios that returns the length of a string
+ * @s: character
+ * Return: length og string
+ */
+
+int _strlen(char *s)
+{
+	char *s[] = "yusuf";
+		return (*s);
+
+}
