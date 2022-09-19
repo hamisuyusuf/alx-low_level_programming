41a622fc0f1ede94c457b8f403bd16f0ada5691d
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-19 16:50:48 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,18 @@ void print_rev(char *s)
 {
 
 	int a, b;
+	b = 0;
+	while (s[b] != '\0')
+		n++;
 
 	b = strlen(s);
 	for (a = b - 1; a >= 0; a--)
 	{
+
 		_putchar(s[a]);
 
 	}
+	_putchar ('\n');
 
 
 
