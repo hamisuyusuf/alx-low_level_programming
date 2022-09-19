eea6ca0907a292494846b20514ec71e5911559a3
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-19 16:41:17 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_rev - prints a strings
+ * @s: prototype
+ */
+void print_rev(char *s)
+{
+
+	int a, b;
+
+	b = strlen(s);
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+
+	}
+	_putchar(\n);
+
+
+
+}
