c27c55235bb829b0f59893d073e3a02a7c7c4820
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-19 17:29:33 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,14 @@ void puts2(char *str)
 	int a;
 
 	for (a = 0; str[a] != '\0'; ++a)
+
 	{
-	  if (a % 2==0)
-		_putchar(str[a]);
+
+		if (a % 2 == 0)
+			_putchar (str[a]);
 	}
-	_putchar('\n');
+
+	_putchar ('\n');
 
 }
+
