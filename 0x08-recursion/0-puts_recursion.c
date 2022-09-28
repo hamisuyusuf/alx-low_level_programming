ed5a9a99f82dfc518e4103af8c47cd47e036477e
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-09-28 04:45:21 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @char: first char
+ * @s: poniter
+ * Return: new line
+ */
+
+void _puts_recursion(char *s);
+{
+	char *s = "hello";
+
+	_putchar(s);
+
+void _puts_recursion(char *s)
+{
+	_puchar(\n);
+
+}
+
+}
