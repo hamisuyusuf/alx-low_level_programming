82fda3e62c431827ac377cf3a55f2944ffba6f33
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-10-06 04:34:31 -0700

    Task 0

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * malloc_checked - alloctes memory using malloc
+ * @b: number of blocks required
+ * Description:
+ * Return: pointer to allocated memory
+ **/
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	if (b <= 0)
+	{
+		return;
+	}
+	a = (void *)malloc(b);
+	if (a = NULL)
+	{
+		return;
+	}
+	return (a);
+}
