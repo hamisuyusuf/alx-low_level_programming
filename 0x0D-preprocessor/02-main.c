4651eec824eb97277a566c63d0fc4227b5769db6
hamisuyusuf <hamisuyusuf180@gmail.com>
2022-10-10 06:28:33 -0700

    Task 2

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main: entry point
+ *
+ * Description: Write a program that prints the name of the file it was compiled from, followed by a new line
+ *
+ * Return: nothing
+ */
+int main()
+{
+
+	printf( __FILE__);
+	return (0);
+}
