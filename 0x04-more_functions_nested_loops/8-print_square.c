afe5abe40fdabdce595b787ec9c87b4e57c641d3
hamisuyusuf <hamisuyusuf180@gmail.com>
2024-05-13 03:40:01 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: parameter
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	while (i < size && size > 0)
+	{
+		int j = 0;
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
