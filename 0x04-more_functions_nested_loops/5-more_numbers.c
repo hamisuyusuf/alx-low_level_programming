bb395b0f593d84db88349164b3fd50afae7bae46
hamisuyusuf <hamisuyusuf180@gmail.com>
2024-05-13 02:11:29 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: number 10 times
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
